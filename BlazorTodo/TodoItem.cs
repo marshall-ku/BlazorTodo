3c43e9ea68ce01bd93576a1a677aaca34db50ece
marshall-ku <marshall@kakao.com>
2022-01-12 18:10:50 +0900

    Implement todo item removing

--- BlazorTodo/TodoItem.cs
+++ BlazorTodo/TodoItem.cs
@@ -2,6 +2,7 @@
 {
     public class TodoItem
     {
+        public long Id { get; set; }
         public string? Title { get; set; }
         public string? Description { get; set; }
         public bool IsDone { get; set; }
